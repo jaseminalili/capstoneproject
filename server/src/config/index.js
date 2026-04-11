19ccb1002da90865261e69f9b4cf4e0f3f619b8a
jaseminalili <ja30149@seeu.edu.mk>
2026-04-11 06:49:29 +0200

    vercel backend support

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -31,7 +31,7 @@ const config = {
   },
 
   cors: {
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    origin:      process.env.CLIENT_ORIGIN ? process.env.CLIENT_ORIGIN.split(',').map(o => o.trim()) : ['http://localhost:5173'],
     credentials: true,
   },
 
