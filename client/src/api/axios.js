25f6d0fc2fa7cc2396f273211433ce070f771dc5
jaseminalili <ja30149@seeu.edu.mk>
2026-04-11 03:37:38 +0200

    fixed axios.js file

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,6 +5,7 @@ const api = axios.create({
   baseURL: (import.meta.env.VITE_API_URL || '/api').replace(/\/$/, '') + '/',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 })
 
 // Attach JWT on every request
